74a6cd8020330e99f43b58ac1bea114be844429a
Manoj S <134809278+manojscoder@users.noreply.github.com>
2023-06-18 22:25:19 +0530

    This C program will add a node in the given position

---
+++ Linked_list/Circular_Doubly_Linked_List/Add_middle.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    struct node* prev;
+    int data;
+    struct node* next;
+};
+struct node* addtoempty(int data)
+{
+    struct node* tail=malloc(sizeof(struct node));
+    tail->prev=tail;
+    tail->data=data;
+    tail->next=tail;
+    return tail;
+}
+struct node* addatbegin(struct node* tail,int data)
+{
+    if(tail->prev==tail)
+    {
+        struct node* temp=malloc(sizeof(struct node));
+        temp->prev=tail;
+        temp->data=data;
+        temp->next=tail->next;
+        tail->next=temp;
+        tail->prev=temp;
+        return tail;
+    }
+    else
+    {
+    struct node* temp=malloc(sizeof(struct node));
+    temp->prev=tail;
+    temp->data=data;
+    temp->next=tail->next;
+    tail->next=temp;
+    tail->next->prev=temp;
+    return tail;
+    }
+}
+struct node* addatend(struct node* tail,int data)
+{
+    struct node* temp=malloc(sizeof(struct node));
+    temp->prev=tail;
+    temp->data=data;
+    temp->next=tail->next;
+    tail->next->prev=temp;
+    tail->next=temp;
+    tail=temp;
+    return tail;
+}
+struct node* addatpos(struct node* tail,int pos,int data)
+{
+    struct node* temp=malloc(sizeof(struct node));
+    if(pos==1)
+    {
+        temp->prev=tail;
+        temp->data=data;
+        temp->next=tail->next;
+        tail->next->prev=temp;
+        tail->next=temp;
+        return tail;
+    }
+    else{
+        struct node* temp1=tail->next;
+        pos--;
+        while(pos!=1)
+        {
+            temp1=temp1->next;
+            pos--;
+        }
+        temp->prev=temp1;
+        temp->data=data;
+        temp->next=temp1->next;
+        temp1->next->prev=temp;
+        temp1->next=temp;
+        if(temp->prev==tail)
+        {
+            return tail->next;
+        }
+        else
+        return tail;
+    }
+}
+int main()
+{
+    struct node* tail=NULL;
+    tail=addtoempty(45);
+    tail=addatbegin(tail,34);
+    tail=addatend(tail,56);
+    tail=addatpos(tail,4,23);
+    struct node* ptr=tail->next;
+    printf("%d ",ptr->data);
+    ptr=ptr->next;
+    while(ptr!=tail->next)
+    {
+        printf("%d ",ptr->data);
+        ptr=ptr->next;
+    }
+    
+}
